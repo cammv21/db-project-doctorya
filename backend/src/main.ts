83192d2e7c0c3f783c45aeba78e74071eef886f7
Camilo Muñoz <juanc.munozv@autonoma.edu.co>
2024-11-14 19:32:48 -0500

    feat(backend): change backend framework to nestjs

---
+++ backend/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Swagger Configuration
+  // const config = new DocumentBuilder()
+  //   .setTitle('Doctor YA API')
+  //   .setDescription('An API for our database project')
+  //   .setVersion('1.0')
+  //   .build();
+    
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
